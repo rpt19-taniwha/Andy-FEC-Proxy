c95dc691d96500f5af61b37ef1d8ae49184c198f
Andy Gala <livelifebrands@gmail.com>
2020-04-01 12:59:41 -0700

    Render Mervin's service

--- server/server.js
+++ server/server.js
@@ -2,18 +2,19 @@ const express = require('express');
 const path = require('path');
 
 const bodyParser = require('body-parser');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-const cors = require('cors');
 const app = express();
 
-app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/', createProxyMiddleware({ target: 'http://localhost:8080', changeOrigin: true }));
+app.use(express.static(path.join(__dirname, '/../client/dist/')));
+
+app.get('/listing/:productNumber', (req, res) => {
+  res.sendFile('index.html', {
+    root: path.join(__dirname, '/../client/dist/'),
+  });
+});
 
 const port = process.env.PORT || 8000;
-
 app.listen(port, () => console.log(`Listening on port ${port}`));
